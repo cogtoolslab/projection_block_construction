5924bb384cff8c1984c63e548727da7ab2ea8dba
Felix Binder <me@felixbinder.net>
2020-05-02 23:05:20 +0200

    Partial implementation of blockworld
    Still needs stability testing, integration with agent

---
+++ projection_agent/blockworld.py
@@ -0,0 +1,340 @@
+from world import World
+
+import numpy as np
+from PIL import Image
+
+from matplotlib import pylab, mlab, pyplot
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from matplotlib.path import Path
+import matplotlib.patches as patches
+import copy
+import json
+import datetime
+import random
+from random import randint
+import string
+import os
+
+# import displayworld
+
+class Blockworld(World):
+    """This class implements the blockworld as defined in https://github.com/cogtoolslab/block_construction. It manages state, allows for transition and scoring function of states both by F1 score and stability. Stability is calculated using the box2d as opposed to matter in the browser version. The stable/unstable distinction in the cases here is simple enough that differences between physics engines should not matter. 
+    
+    All the important functions are implemented by the State class, which represents a particular configuration of the world. The world merely manages the current state and wraps around the functions of the class State to keep compatibility with previous uses of the class.
+    
+    Default values are chosen to correspond to the silhouette 2 study, see block_construction/experiments/silhouette_2.
+    
+    Dimensions are in y,x. The origin is top left (in accordance with numpy arrays."""
+
+    def __init__(self,dimension = (13,18),silhouette=None,block_library = None):
+        self.dimension = dimension
+        #Defines dimensions of possible blocks. 
+        if block_library is None: #the default block library is the one from the silhouette 2 study
+            block_library = self._silhouette2_default_blocklibrary()
+        self.block_library = block_library 
+        #load the target silhouette as numpy array
+        self.silhouette = self.load_silhouette(silhouette) 
+        self.current_state = Blockworld.State(self,[]) #generate a new state with no blocks in it
+
+    def _silhouette2_default_blocklibrary(self):
+    #The defaults are taken from the silhouette2 study.
+        return [BaseBlock(1,2),
+                    BaseBlock(2,1),
+                    BaseBlock(2,2),
+                    BaseBlock(2,4),
+                    BaseBlock(4,2),
+                    ] 
+
+    def  transition(self,action,state=None):
+        """Takes an action and a state and returns the resulting state."""
+        if state is None:
+            state = self.current_state
+        if action not in self.possible_actions(state): #this might be slow and could be taken out for performance sake
+            print("Action is not in possible actions")
+        #determine where the new block would land
+        baseblock, x = action #unpacking the action
+        #determine y coordinates of block
+        y = 0
+        while y < self.dimension[0] and state.block_map[y,range(x, x + baseblock.width)].sum() ==  0: #find the lowest free row in the tower 
+            y+= 1
+        y = y-1 #because y marks the first full row
+        #create new block
+        new_block = Block(baseblock,x,y)
+        #create new state
+        new_state = Blockworld.State(self,state.blocks + [new_block])
+        return new_state
+
+
+    def load_silhouette(self,silhouette):
+        if type(silhouette) is np.ndarray:
+            if silhouette.shape == self.world.dimension:
+                return silhouette
+            else:
+                raise Warning("Silhouette dimensions don't match world dimensions")
+        elif silhouette is None: #No silhouette returns an empty field.
+            return np.zeros(self.dimension)
+        else:
+            #TODO implement importing from file
+            raise Warning("Importing from file not implemented yet")
+
+
+    """Simple functions inherited from the class World"""
+    def apply_action(self,action):
+        self.current_state = self.transition(action,self.current_state)
+
+    def is_fail(self,state = None):
+        if state is None:
+            state = self.world_state
+        if state.stability() is False: #we loose if its unstable
+            return True
+        if state.F1score() != 1 and state.possible_actions == []: #we loose if we aren't finished and have no options
+            return True
+        return False
+
+    def is_win(self,state = None):
+        if state is None:
+            state = self.world_state
+        if state.F1score == 1 and state.stability():
+            return True
+        return False
+
+    def score(self,state=None):
+        if state is None:
+            state = self.world_state
+        if self.is_fail(state):
+            return self.fail_penalty
+        if self.is_win(state):
+            return self.win_reward
+        return state.F1score()
+
+    def possible_actions(self,state=None):
+        if state is None:
+            state = self.current_state
+        return state.possible_actions()
+
+    def stability(self,state=None):
+        if state is None:
+            state = self.world_state
+        return state.stability()
+
+
+
+    class State():
+        """This subclass contains a (possible or current) state of the world and implements a range of functions to score it, namely for F1 (how much of the figure are we filling out) and physical stability. It also generates possible actions. The blockworld classes wrap around this class"""
+        def __init__(self,world,blocks):
+            self.world = world
+            self.blocks = blocks
+            self.world_width = self.world.dimension[1]
+            self.world_height = self.world.dimension[0]
+            self.block_map = np.zeros((self.world_height, self.world_width),dtype=int) ## bitmap for placement of blocks
+            self._update_map_with_blocks(blocks) #read the blocks into the blockmap
+        
+        def _update_map_with_blocks(self, blocks, delete=False):
+            """Fills the blockmap with increasing numbers for each block. 0 is empty space. Original blockmap behavior can be achieved by blockmap > 0."""
+            for b in blocks:
+                new_number = 0 if delete else (np.max(self.block_map)+1) #numbers increase 
+                self.block_map[(b.y-b.height)+1: b.y+1, b.x:(b.x+b.width)] = new_number 
+
+        def F1score(self):
+            """Returns the F1 score relative to the target silhoutte defined in the corresponding world. If the silhouette is empty, this produces division by 0 errors and returns NaN."""
+            target = self.world.silhouette > 0
+            built = self.block_map > 0
+            precision = np.sum(built & target)/np.sum(built) 
+            recall = np.sum(built & target)/np.sum(target) 
+            F1score = 2 * (precision * recall)/(precision + recall)
+            return F1score
+
+        def stability(self):
+            """Runs physics engine to determine stability. Returns true or false, but could be adapted for noisy simulations."""
+            return True
+
+        def possible_actions(self):
+            """Generates all actions that are possible in this state independent of whether the block is stable or within the silhouette. Simply checks whether the block is in bounds. 
+            Format of action is (BaseBlock from block_library, x location of lower left)."""
+            block_library = self.world.block_library
+            possible_actions = []
+            for base_block in block_library:
+                for x in range(self.world_width): #starting coordinate is bottom left
+                    #check whether it overlaps the left boundary
+                    if x + base_block.width < self.world_width:
+                         #and whether it overlaps the top boundary by simply looking if the block at the top is free
+                        if self.block_map[0 : base_block.height, x : x+base_block.width] .sum() == 0:
+                            possible_actions.append((base_block,x))
+            return possible_actions
+
+        def visual_display(self):
+            """Shows the state in a pretty way."""
+            pyplot.imshow(self.block_map, cmap='hot_r')
+            pyplot.show()
+
+class Block:
+    '''
+        Adapted from block_construction/stimuli/blockworld_helpers.py
+        Creates Block objects that are instantiated in a world
+        x and y define the position of the BOTTOM LEFT corner of the block
+        
+        Defines functions to calculate relational properties between blocks
+    '''
+    def __init__(self, base_block, x, y):
+        self.base_block = base_block # defines height, width and other functions
+        #bottom left coordinate
+        self.x = x
+        self.y = y
+        self.height = base_block.height
+        self.width = base_block.width
+        self.verts = base_block.translate(base_block.base_verts,x,y)
+    
+    #Block Relational Properties
+    def above(self, other):
+        ''' Test whether this block is fully above another block.
+        
+            Returns true iff the height of the bottom face of this block 
+            is greater than or equal to the top face of other block.
+        '''
+        return (self.y >= other.y + other.height)
+    
+    def below(self, other):
+        ''' Test whether this block is fully below another block.
+            
+            Returns true iff the height of the top face of this block 
+            is less than or equal to the bottom face of other block.
+        '''
+        return (self.y + self.height <= other.y)
+    
+    def leftof(self, other):
+        ''' Test whether this block is fully to the left of another block
+        
+            Returns true iff the height of the bottom face of this block 
+            is greater than or equal to the top face of other block.
+        '''
+        return (self.x + self.width <= other.x)
+    
+    def rightof(self, other):
+        ''' Test whether this block is fully to the right of another block.
+            
+            Returns true iff the height of the top face of this block 
+            is less than or equal to the bottom face of other block.
+        '''
+        return (self.x >= other.x + other.width)
+    
+    def sides_touch(self, other):
+        ''' Test to see whether this block sits touching sides of another block.
+            Corner to corner treated as not touching.
+        '''
+        y_overlap = not self.above(other) and not self.below(other) 
+        buttressing_side = self.x == other.x + other.width or other.x == self.x + self.width
+        return y_overlap and buttressing_side
+    
+    def vertical_touch(self, other):
+        ''' Test to see whether this block sits top to bottom against other block.
+            Corner to corner treated as not touching.
+        '''
+        x_overlap = not self.leftof(other) and not self.rightof(other) 
+        buttressing_up = self.y == other.y + other.height or other.y == self.y + self.height
+        return x_overlap and buttressing_up
+    
+    def touching(self, other):
+        ''' Test to see if this block is touching another block.
+            Corner to corner treated as not touching.
+        '''
+        return self.sides_touch(other) or self.vertical_touch(other)        
+
+    def abs_overlap(self, other, horizontal_overlap=True):
+        ''' horizontal- are we measuring horizontal overlap?
+        '''
+        if horizontal_overlap and self.vertical_touch(other):
+            return min([abs(self.x + self.width - other.x), abs(other.x + other.width - self.x)])
+        elif not horizontal_overlap and self.sides_touch(other):
+            return min([abs(self.y + self.height - other.y), abs(other.y + other.height - self.y)])
+        else:
+            return 0
+        
+    def partially_supported_by(self, other):
+        ''' True if the base of this block is touching the top of the other block 
+        '''
+        return self.above(other) and (self.abs_overlap(other) > 0)
+    
+    def completely_supported_by(self, other):
+        ''' True if the whole of the base of this block is touching the top of the other block 
+        '''
+        return self.above(other) and (self.abs_overlap(other) == self.width)
+    
+    def dual_supported(self, block_a, block_b):
+        ''' Is this block partially supported by both blocks a and b?
+        '''
+        return self.partially_supported(block_a) and self.partially_supported(block_b)
+    
+    
+    '''
+    Other useful properties:
+    - 
+    
+    '''
+
+class BaseBlock:
+    '''
+    Base Block class for defining a block object with attributes.             
+    Adapted from block_construction/stimuli/blockworld_helpers.py
+
+    '''
+    
+    def __init__(self, width=1, height=1, shape='rectangle', color='gray'):
+        self.base_verts = np.array([(0, 0), 
+                            (0, 1 * height), 
+                            (1 * width, 1 * height), 
+                            (1 * width, 0), 
+                            (0,0)])
+        self.width = width
+        self.height = height
+        self.shape = shape
+        self.color = color
+    
+    def __str__(self):
+        return(str(self.width) + 'x' + str(self.height))
+
+    def init(self):
+        self.corners = self.get_corners(self.base_verts)
+        self.area = self.get_area(shape=self.shape) 
+        
+    def translate(self, base_verts, dx, dy):
+        '''
+        input:
+            base_verts: array or list of (x,y) vertices of convex polygon. 
+                    last vertex = first vertex, so len(base_verts) is num_vertices + 1
+            dx, dy: distance to translate in each direction
+        output:
+            new vertices
+        '''
+        new_verts = copy.deepcopy(base_verts)
+        new_verts[:,0] = base_verts[:,0] + dx
+        new_verts[:,1] = base_verts[:,1] + dy
+        return new_verts
+
+    def get_corners(self,base_verts):
+        '''
+        input: list or array of block vertices in absolute coordinates
+        output: absolute coordinates of top_left, bottom_left, bottom_right, top_right
+        '''
+        corners = {}
+        corners['bottom_left'] = verts[0]
+        corners['top_left'] = verts[1]
+        corners['top_right'] = verts[2]
+        corners['bottom_right'] = verts[3]
+        return corners
+
+    def get_area(self,shape='rectangle'):
+        '''
+        input: w = width 
+            h = height           
+            shape = ['rectangle', 'square', 'triangle']
+        output
+        '''
+        ## extract width and height from dims dictionary 
+        if shape in ['rectangle','square']:
+            area = self.width*self.height
+        elif shape=='triangle':
+            area = self.width*self.height*0.5
+        else:
+            print('Shape type not recognized. Please use recognized shape type.')
+        return area   
--- projection_agent/main.py
+++ projection_agent/main.py
@@ -1,11 +1,25 @@
 from agent import Agent
-from world import Stonehenge
+import blockworld
+import random
 
-w = Stonehenge()
+w = blockworld.Blockworld()
 a = Agent(w,1)
-# ast = a.build_ast()
-# a.score_ast(ast)
-# ast.print_tree()
-# act_seqs = a.generate_action_sequences(ast,include_subsets=True)
-# a.score_action_sequences(act_seqs,'Final state',True)
-a.act(2,verbose=True,scoring='Fixed')
+s = w.current_state
+print(s.possible_actions())
+print(s.F1score())
+# for i in range(7):
+#     print(i)
+#     w.apply_action((blockworld.BaseBlock(4,2),7))
+#     print(w.current_state.block_map)
+# while w.possible_actions() != []:
+#     print(w.current_state.block_map)
+#     a = random.choice(w.possible_actions())
+#     print(a[0].__str__(),a[1])
+#     w.apply_action(a)
+# w.current_state.visual_display()
+
+
+
+ast = a.build_ast()
+a.score_ast(ast)
+ast.print_tree()
