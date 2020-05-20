ff53cb3e6a547079a3c4da10801a0203e933211a
Felix Binder <me@felixbinder.net>
2020-05-20 15:42:13 +0200

    Two simple test cases of an agent that does stonehenge

--- projection_agent/main.py
+++ projection_agent/main.py
@@ -1,33 +1,15 @@
 from agent import Agent
 import blockworld
 import random
-from silhuouettes import stonehenge
+import blockworld_library as bl
 
-w = blockworld.Blockworld(silhouette=stonehenge)
-a = Agent(w,1)
-s = w.current_state
-print(s.possible_actions())
-print(s.F1score())
-print(s.stability())
+w = blockworld.Blockworld(silhouette=bl.stonehenge_6_4,dimension=(6,4),block_library = bl.stonehenge_6_4_blocklibrary)
+a = Agent(w,sparse=False)
+#greedy agent
+while w.status() == 'Ongoing':
+    a.act(1,verbose=True)
 
-for i in range(25):
-    print("Iteration:",i)
-    print(w.current_state.block_map)
-    a = random.choice(w.possible_actions())
-    print(a[0].__str__(),a[1])
-    w.apply_action(a)
-    print(w.current_state.block_map)
-    w.current_state.visual_display(blocking=False)
-    print("Stability: ",w.current_state.stability(visual_display=True))
-# while w.possible_actions() != []:
-#     print(w.current_state.block_map)
-#     a = random.choice(w.possible_actions())
-#     print(a[0].__str__(),a[1])
-#     w.apply_action(a)
-w.current_state.visual_display()
-
-
-
-# ast = a.build_ast()
-# a.score_ast(ast)
-# ast.print_tree()
+#brute force search agent
+w = blockworld.Blockworld(silhouette=bl.stonehenge_6_4,dimension=(6,4),block_library = bl.stonehenge_6_4_blocklibrary)
+a = Agent(w,sparse=False)
+a.act(6,verbose=True)
