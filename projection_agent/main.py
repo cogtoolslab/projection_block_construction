791c6f8903c2257022724433be9a7afee96efa02
Felix Binder <me@felixbinder.net>
2020-05-29 22:44:51 +0200

    MCTS Test

--- projection_agent/main.py
+++ projection_agent/main.py
@@ -8,16 +8,16 @@ import experiment_runner
 import time
 start_time = time.time()
 
-# a = MCTS_Agent(horizon=1000)
-# w = bw.Blockworld(silhouette=bl.load_interesting_structure(15),block_library=bl.bl_silhouette2_default)
-# a.set_world(w)
-# while w.status() == 'Ongoing':
-#     a.act(-1,verbose=True)
+a = MCTS_Agent(horizon=100000)
+w = bw.Blockworld(silhouette=bl.load_interesting_structure(15),block_library=bl.bl_silhouette2_default)
+a.set_world(w)
+while w.status() == 'Ongoing':
+    a.act(-1,verbose=True)
 
-agents = [Agent(horizon=1,scoring_function=bw.F1score),Agent(horizon=1,scoring_function=bw.silhouette_hole_score),Agent(horizon=3,scoring_function=bw.silhouette_hole_score),Agent(horizon=5,scoring_function=bw.silhouette_hole_score),MCTS_Agent(horizon=1000)]
-silhuouettes = [bl.load_interesting_structure(i) for i in range(16)]
-worlds = [bw.Blockworld(silhouette=s,block_library=bl.bl_silhouette2_default) for s in silhuouettes]
-results = experiment_runner.run_experiment(worlds,agents,1,60,verbose=False)
-print(results[['world','outcome']])
+# agents = [Agent(horizon=1,scoring_function=bw.F1score),Agent(horizon=1,scoring_function=bw.silhouette_hole_score),Agent(horizon=3,scoring_function=bw.silhouette_hole_score),Agent(horizon=5,scoring_function=bw.silhouette_hole_score),MCTS_Agent(horizon=1000)]
+# silhuouettes = [bl.load_interesting_structure(i) for i in range(16)]
+# worlds = [bw.Blockworld(silhouette=s,block_library=bl.bl_silhouette2_default) for s in silhuouettes]
+# results = experiment_runner.run_experiment(worlds,agents,1,60,verbose=False)
+# print(results[['world','outcome']])
 
 print("Done in %s seconds" % (time.time() - start_time))
