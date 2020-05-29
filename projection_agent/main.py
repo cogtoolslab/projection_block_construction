76e181596e5226160f2567fee7c8b925bb707b3e
Felix Binder <me@felixbinder.net>
2020-05-29 22:24:23 +0200

    Test case

--- projection_agent/main.py
+++ projection_agent/main.py
@@ -14,7 +14,7 @@ start_time = time.time()
 # while w.status() == 'Ongoing':
 #     a.act(-1,verbose=True)
 
-agents = [Agent(horizon=1,scoring_function=bw.F1score),Agent(horizon=1,scoring_function=bw.silhouette_hole_score),MCTS_Agent(horizon=10000)]
+agents = [Agent(horizon=1,scoring_function=bw.F1score),Agent(horizon=1,scoring_function=bw.silhouette_hole_score),Agent(horizon=3,scoring_function=bw.silhouette_hole_score),Agent(horizon=5,scoring_function=bw.silhouette_hole_score),MCTS_Agent(horizon=1000)]
 silhuouettes = [bl.load_interesting_structure(i) for i in range(16)]
 worlds = [bw.Blockworld(silhouette=s,block_library=bl.bl_silhouette2_default) for s in silhuouettes]
 results = experiment_runner.run_experiment(worlds,agents,1,60,verbose=False)
