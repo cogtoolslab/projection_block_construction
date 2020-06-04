1232c5f6da191b7a772bca1eadd93a9ee6580469
Felix Binder <me@felixbinder.net>
2020-06-04 20:00:10 +0200

    New tests

--- projection_agent/main.py
+++ projection_agent/main.py
@@ -28,7 +28,7 @@ agents = [
     Agent(horizon=4,scoring_function=bw.silhouette_hole_score),
     Agent(horizon=5,scoring_function=bw.silhouette_hole_score),
     MCTS_Agent(horizon=1000),
-    MCTS_Agent(horizon=5000),
+    MCTS_Agent(horizon=2500),
     # MCTS_Agent(horizon=10000),
     ]
 silhouettes = [bl.load_interesting_structure(i) for i in range(16)]
