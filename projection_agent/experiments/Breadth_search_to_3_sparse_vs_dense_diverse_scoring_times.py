fec3bc6b1a3de9658e6dc9a45a7bc222777fcc70
Felix Binder <me@felixbinder.net>
2020-06-06 21:36:05 +0200

    Experiment

--- projection_agent/experiments/Breadth_search_to_3_sparse_vs_dense_diverse_scoring_times.py
+++ projection_agent/experiments/Breadth_search_to_3_sparse_vs_dense_diverse_scoring_times.py
@@ -63,7 +63,7 @@ agents = [
     ]
 
 silhouettes = {i : bl.load_interesting_structure(i) for i in [14,15,5,8,12,1]}
-worlds_silhouettes = {'int_struct_'+str(i) : bw.Blockworld(silhouette=s,block_library=bl.bl_silhouette2_default) for i,s in silhouettes}
+worlds_silhouettes = {'int_struct_'+str(i) : bw.Blockworld(silhouette=s,block_library=bl.bl_silhouette2_default) for i,s in silhouettes.items()}
 worlds_small = {
     'stonehenge_6_4' : bw.Blockworld(silhouette=bl.stonehenge_6_4,block_library=bl.bl_stonehenge_6_4),
     'stonehenge_3_3' : bw.Blockworld(silhouette=bl.stonehenge_3_3,block_library=bl.bl_stonehenge_3_3),
@@ -71,7 +71,7 @@ worlds_small = {
     'T' : bw.Blockworld(silhouette=bl.T,block_library=bl.bl_stonehenge_6_4),
     'side_by_side' : bw.Blockworld(silhouette=bl.side_by_side,block_library=bl.bl_stonehenge_6_4),
 }
-worlds = worlds_silhouettes+worlds_small
+worlds = {**worlds_silhouettes,**worlds_small}
 results = experiment_runner.run_experiment(worlds,agents,10,60,verbose=False,parallelized=fraction_of_cpus,save='breadth_to_3')
 print(results[['agent','world','outcome']])
 
