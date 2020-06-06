3cd38ab934bc63f774d7fb0397c96c8aa745092d
Felix Binder <me@felixbinder.net>
2020-06-06 18:45:21 +0200

    Fixed paths

--- projection_agent/experiments/benchmarks.py
+++ projection_agent/experiments/benchmarks.py
@@ -1,3 +1,12 @@
+#add the parent path to import the modules
+from inspect import getsourcefile
+import os.path
+import sys
+current_path = os.path.abspath(getsourcefile(lambda:0))
+current_dir = os.path.dirname(current_path)
+parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
+sys.path.insert(0, parent_dir)
+
 from agent import Agent
 from MCTS_Agent import MCTS_Agent
 import blockworld as bw
