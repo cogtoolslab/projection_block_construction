ce46f163bf32d46b4009cf37e813376f85e3d01c
Felix Binder <me@felixbinder.net>
2020-10-31 23:05:38 +0100

    new ways of moving cp in experiment

--- experiments/paired_tool_notool_BFS_experiment.py
+++ experiments/paired_tool_notool_BFS_experiment.py
@@ -23,12 +23,27 @@ if __name__=="__main__": #required for multiprocessing
     fraction_of_cpus = 0.375
 
     agents = [
+        #default
         BFS_Agent(only_improving_actions = True,horizon = 1, scoring_function=bw.random_scoring),
         CPA.Construction_Paper_Agent(decomposition_function=CPA.random_1_4,lower_agent=BFS_Agent(only_improving_actions = True,horizon = 1, scoring_function=bw.random_scoring)),
         BFS_Agent(only_improving_actions = True,horizon = 1),
         CPA.Construction_Paper_Agent(decomposition_function=CPA.random_1_4,lower_agent=BFS_Agent(only_improving_actions = True,horizon = 1)),
         BFS_Agent(only_improving_actions = True,horizon = 2),
-        CPA.Construction_Paper_Agent(decomposition_function=CPA.random_1_4,lower_agent=BFS_Agent(only_improving_actions = True,horizon = 2))
+        CPA.Construction_Paper_Agent(decomposition_function=CPA.random_1_4,lower_agent=BFS_Agent(only_improving_actions = True,horizon = 2)),
+        #random_2_4
+        # BFS_Agent(only_improving_actions = True,horizon = 1, scoring_function=bw.random_scoring),
+        CPA.Construction_Paper_Agent(decomposition_function=CPA.random_1_4,lower_agent=BFS_Agent(only_improving_actions = True,horizon = 1, scoring_function=bw.random_scoring),decomposition_function = CPA.random_2_4),
+        # BFS_Agent(only_improving_actions = True,horizon = 1),
+        CPA.Construction_Paper_Agent(decomposition_function=CPA.random_1_4,lower_agent=BFS_Agent(only_improving_actions = True,horizon = 1),decomposition_function = CPA.random_2_4),
+        # BFS_Agent(only_improving_actions = True,horizon = 2),
+        CPA.Construction_Paper_Agent(decomposition_function=CPA.random_1_4,lower_agent=BFS_Agent(only_improving_actions = True,horizon = 2),decomposition_function = CPA.random_2_4),
+        #fixed_2
+        # BFS_Agent(only_improving_actions = True,horizon = 1, scoring_function=bw.random_scoring),
+        CPA.Construction_Paper_Agent(decomposition_function=CPA.random_1_4,lower_agent=BFS_Agent(only_improving_actions = True,horizon = 1, scoring_function=bw.random_scoring),decomposition_function = CPA.fixed_2),
+        # BFS_Agent(only_improving_actions = True,horizon = 1),
+        CPA.Construction_Paper_Agent(decomposition_function=CPA.random_1_4,lower_agent=BFS_Agent(only_improving_actions = True,horizon = 1),decomposition_function = CPA.fixed_2),
+        # BFS_Agent(only_improving_actions = True,horizon = 2),
+        CPA.Construction_Paper_Agent(decomposition_function=CPA.random_1_4,lower_agent=BFS_Agent(only_improving_actions = True,horizon = 2),decomposition_function = CPA.fixed_2),
         ]
 
     silhouette8 = [14,11,3,13,12,1,15,5]
