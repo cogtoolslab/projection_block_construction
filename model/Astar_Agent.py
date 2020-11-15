a9bb7cf298c9edcb20b5b5f8b2312cb6f7a7b0b5
Felix Binder <me@felixbinder.net>
2020-11-15 16:54:12 +0100

    Astar is now stochastic

--- model/Astar_Agent.py
+++ model/Astar_Agent.py
@@ -6,7 +6,7 @@ sys.path.insert(0,proj_dir)
 from model.BFS_Agent import BFS_Agent, Ast_node, Ast_edge
 from model.Beam_Search_Agent import backtrack
 import utils.blockworld as blockworld
-import queue
+import random
 from dataclasses import dataclass, field
 from typing import Any
 from statistics import mean
@@ -29,22 +29,25 @@ class Astar_Agent(BFS_Agent):
     This is a simplified implementation that doesn't take into account that the same state can be reached in multiple ways. However, because we defined the cost function as number of steps, every state can only be reached in the same number of steps (since taking more steps means placing more blocks, and different sizes of blocks lead to potentially different stability), and therefore there cannot be a better path to a node in open set, just an equivalently good one. 
     """
 
-    def __init__(self,world = None,heuristic=blockworld.F1_stability_score,max_steps=10**6, only_improving_actions = False):
+    def __init__(self,world = None,heuristic=blockworld.F1_stability_score,max_steps=10**6, only_improving_actions = False,random_seed=None):
         self.world = world
         self.heuristic = heuristic
         self.max_steps = max_steps
         self.only_improving_actions = only_improving_actions
+        self.random_seed = random_seed
+        if self.random_seed is None: self.random_seed = random.randint(0,99999)
 
     def __str__(self):
         """Yields a string representation of the agent"""
-        return self.__class__.__name__+' heuristic: '+self.heuristic.__name__+' max_steps '+str(self.max_steps)
+        return self.__class__.__name__+' heuristic: '+self.heuristic.__name__+' max_steps '+str(self.max_steps)+' random seed: '+str(self.random_seed)
     
     def get_parameters(self):
         """Returns dictionary of agent parameters."""
         return {
             'agent_type':self.__class__.__name__,
             'heuristic':self.heuristic.__name__,
-            'max_steps':self.max_steps
+            'max_steps':self.max_steps,
+            'random_seed':self.random_seed
             }    
 
     def act(self,steps = None, verbose = False):
@@ -77,11 +80,11 @@ class Astar_Agent(BFS_Agent):
         root = Ast_node(self.world.current_state)
         i = 0
         number_of_states_evaluated = 0
-        open_set = queue.PriorityQueue() #using a priority queue to manage the states
+        open_set = Stochastic_Priority_Queue() #using a priority queue to manage the states
         open_set.put(FringeNode(self.f(root),root))
         while not open_set.empty() and i != self.max_steps:
             i+=1
-            current_node = open_set.get().node #get node with lowest projected cost. This removes it from the open set
+            current_node = open_set.get() #get node with lowest projected cost. This removes it from the open set
             #check if that node is winning
             if self.world.is_win(current_node.state): #🎉
                 if verbose: print("Found winning state after",i)
@@ -120,3 +123,76 @@ class Astar_Agent(BFS_Agent):
         heur = self.heuristic(node.state)
         out = -(heur-1) * self._silhouette_size / self._avg_block_size
         return max(0,out) #return 0 if the cost to goal is less than 0 (which doesn't make sense)
+
+class Stochastic_Priority_Queue:
+    """Implements a priority queue that randomly returns one of the elements which has the highest value as opposed to the one that was entered first (which Pythons priority queue does). Implemented as linked list. Follows Pythons priority_queue interface. Expect FringeNodes to be passed."""
+
+    def __init__(self,random_seed=None):
+        self.random_seed = random_seed
+        if self.random_seed is None: self.random_seed = random.randint(0,99999)
+        self.head = None #first element of the list
+        self.size = 0
+
+    def get(self):
+        "Return the content of one of the elements that has lowest priority randomly"
+        #find all elements with the lowest cost
+        elems = []
+        cur = self.head
+        # if cur is None: #empty queue
+        #     return None
+        while cur is not None and cur.priority == self.head.priority:
+            elems.append(cur)
+            cur = cur.next
+        random.seed(self.random_seed) #fix random seed
+        ret_elem = random.choice(elems)
+        #take out
+        try:
+            ret_elem.prev.next = ret_elem.next
+        except AttributeError:
+            #update head since the selected on is the head
+            self.head = ret_elem.next
+            pass
+        try:
+            ret_elem.next.prev = ret_elem.prev
+        except AttributeError:
+            pass
+        self.size = self.size - 1 
+        return ret_elem.content
+
+    def put(self,elem):
+        new_elem = _queue_element(elem.node,elem.cost,None,None)
+        #search from top to find insertion place
+        cur = self.head
+        if cur is None: #empty list:
+            self.head = new_elem
+            self.size += 1
+            return
+        if cur.priority > new_elem.priority: #if we're replacing the node
+            cur.prev = new_elem
+            new_elem.next = cur
+            self.head = new_elem
+            self.size += 1
+            return
+        while cur.priority < new_elem.priority and cur.next is not None:
+            cur = cur.next
+        #insert after cur
+        new_elem.next = cur.next
+        cur.next = new_elem
+        new_elem.prev = cur
+        self.size += 1
+
+    def empty(self):
+        return self.size <= 0
+
+    def qsize(self):
+        return self.size
+
+
+class _queue_element:
+    """A single element in the stochastic priority queue. Prev and next are references to other _queue_elements"""
+
+    def __init__(self,content,priority,prev,next):
+        self.content = content
+        self.priority = priority
+        self.prev = prev
+        self.next = next
