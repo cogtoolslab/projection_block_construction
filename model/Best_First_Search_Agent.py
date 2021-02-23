2966e91422338bd2cbc53439c8a68324f9c80c04
Felix Binder <me@felixbinder.net>
2021-02-23 22:24:10 +0100

    Implemented best first search

---
+++ model/Best_First_Search_Agent.py
@@ -0,0 +1,35 @@
+import os
+import sys
+proj_dir =  os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0,proj_dir)
+
+from model.Astar_Agent import Astar_Agent, Stochastic_Priority_Queue
+from model.utils.Search_Tree import *
+import utils.blockworld as blockworld
+import random
+from dataclasses import dataclass, field
+from typing import Any
+from statistics import mean
+
+#class for the priority queue
+@dataclass(order=True)
+class FringeNode:
+    cost: int
+    node: Any=field(compare=False)
+
+class Best_First_Search_Agent(Astar_Agent):
+    """An agent implementing best first search. 
+        Choose heuristic so that higher value means better state (ie F1 score).
+        """
+
+    def __init__(self,world = None,heuristic=blockworld.F1score, only_improving_actions = False, random_seed=None,shuffle=False):
+        self.world = world
+        self.heuristic = heuristic
+        self.only_improving_actions = only_improving_actions
+        self.random_seed = random_seed
+        if self.random_seed is None: self.random_seed = random.randint(0,99999)
+
+    def f(self,node):
+        """Unlike A*, this is just the heuristic.
+        """
+        return self.heuristic(node) * -1
